23b1b0aa1a601b7c46ab4b7406d7d1cd10b75595
Sven-Ola <sven-ola@gmx.de>
2012-04-23 14:49:28 +0200

    Moved checks to AsyncTask

--- app/src/org/dyndns/sven_ola/debian_kit/DebianKitActivity.java
+++ app/src/org/dyndns/sven_ola/debian_kit/DebianKitActivity.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
@@ -19,229 +20,228 @@ import android.widget.TextView;
 
 public class DebianKitActivity extends Activity
 {
-	int ic_cpu;
-	int ic_mem;
-	int ic_fsys;
-	int ic_dev;
-	int ic_mod;
-	int ic_mnt;
-	int ic_deb;
-
-	// Change image for a textview entry
-	private int setDrawable(int res, int ic)
+	private class Verdict
 	{
-		TextView tv = (TextView)findViewById(res);
-		tv.setCompoundDrawablesWithIntrinsicBounds(ic, 0, 0, 0);
-		return ic;
-	}
-
-	// Check if CPU_ABI is arm or x86
-	private int checkCpu()
-	{
-		int ic = R.drawable.ic_maybe;
-		if (0 < android.os.Build.CPU_ABI.length())
-		{
-			if (android.os.Build.CPU_ABI.startsWith("x86") ||
-				android.os.Build.CPU_ABI.startsWith("armeabi"))
-			{
-				ic = R.drawable.ic_passed;
-			}
-			else
-			{
-				ic = R.drawable.ic_failed;
-			}
-		}
-		return setDrawable(R.id.TextView_cpu, ic);
+		public int tv;	// Resource ID of TextView that displays verdict
+		public int ic;	// Resource ID of verdict
+		public Verdict(int tv_resid) {
+	        tv = tv_resid;
+	        ic = -1;
+	    }
 	}
 	
-	// Check if absolute RAM above certain threshold
-	private int checkMem()
+	Verdict v_cpu = new Verdict(R.id.TextView_cpu);
+	Verdict v_mem = new Verdict(R.id.TextView_mem);
+	Verdict v_ext = new Verdict(R.id.TextView_ext);
+	Verdict v_dev = new Verdict(R.id.TextView_dev);
+	Verdict v_mod = new Verdict(R.id.TextView_mod);
+	Verdict v_mnt = new Verdict(R.id.TextView_mnt);
+	Verdict v_deb = new Verdict(R.id.TextView_deb);
+
+	private class CourtTask extends AsyncTask<Void, Verdict, Void>
 	{
-		int ic = R.drawable.ic_unknown;
-		File file = new File("/proc/meminfo");
-		try
-		{
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			// Example: MemTotal: 2050880 kB
-			Pattern p = Pattern.compile("^MemTotal:\\s+(\\d+)\\s+kB$");
-			Matcher m = p.matcher(br.readLine());
-			if (m.find())
+		protected Void doInBackground(Void... voids) {
+			// Check if CPU_ABI is arm or x86
+			v_cpu.ic = R.drawable.ic_maybe;
+			if (0 < android.os.Build.CPU_ABI.length())
 			{
-				int tm=Integer.parseInt(m.group(1));
-				if (tm < 90000)
+				if (android.os.Build.CPU_ABI.startsWith("x86") ||
+					android.os.Build.CPU_ABI.startsWith("armeabi"))
 				{
-					ic = R.drawable.ic_failed;
-				}
-				else if (tm < 120000)
-				{
-					ic = R.drawable.ic_maybe;
+					v_cpu.ic = R.drawable.ic_passed;
 				}
 				else
 				{
-					ic = R.drawable.ic_passed;
+					v_cpu.ic = R.drawable.ic_failed;
 				}
 			}
-		}
-		catch (IOException ex) {}
-		return setDrawable(R.id.TextView_mem, ic);
-	}
+			publishProgress(v_cpu);
 
-	// Check if ext2/3/4 file systems supported
-	private int checkFsys()
-	{
-		int ic = R.drawable.ic_unknown;
-		File file = new File("/proc/filesystems");
-		try
-		{
-			String line;
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			while (null != (line = br.readLine()) && R.drawable.ic_passed != ic)
-			{
-				if (line.matches("\\s+ext[234]$"))
-				{
-					ic = R.drawable.ic_passed;
-				}
-			}
-			if (R.drawable.ic_passed != ic)
-			{
-				ic = R.drawable.ic_failed;
-			}
-		}
-		catch (IOException ex) {}
-		return setDrawable(R.id.TextView_fsys, ic);
-	}
-
-	// Check if kernel modules are supported
-	private int checkMod()
-	{
-		int ic = R.drawable.ic_failed;
-		File file = new File("/proc/modules");
-		if (file.exists())
-		{
-			ic = R.drawable.ic_passed;
-		}
-		
-		return setDrawable(R.id.TextView_mod, ic);
-	}
-
-	// Check if loop-mounts are supported
-	private int checkDev()
-	{
-		int ic = R.drawable.ic_unknown;
-		File file = new File("/proc/devices");
-		try
-		{
-			String line;
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			while (null != (line = br.readLine()) && R.drawable.ic_passed != ic)
-			{
-				if (line.matches("^\\s+7\\s+loop$"))
-				{
-					ic = R.drawable.ic_passed;
-				}
-			}
-			if (R.drawable.ic_passed != ic)
-			{
-				ic = R.drawable.ic_failed;
-			}
-		}
-		catch (IOException ex) {}
-		return setDrawable(R.id.TextView_dev, ic);
-	}
-
-	// Check if /data is not mounted with noexec
-	private int checkMnt()
-	{
-		int ic = R.drawable.ic_unknown;
-		File file = new File("/proc/mounts");
-		try
-		{
-			String line;
-			Pattern p = Pattern.compile(String.format("^\\S+\\s+%s\\s+\\S+\\s+(\\S+)", Environment.getDataDirectory().getAbsolutePath()));
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			while (null != (line = br.readLine()) && R.drawable.ic_passed != ic&& R.drawable.ic_failed != ic)
-			{
-				Matcher m = p.matcher(line);
-				if (m.find())
-				{
-					int i;
-					String[] opts = m.group(1).split(",");
-					for (i = 0; i < opts.length; i++)
-					{
-						if (opts[i].contentEquals("noexec"))
-						{
-							ic = R.drawable.ic_failed;
-						}
-					}
-					if (R.drawable.ic_failed != ic)
-					{
-						ic = R.drawable.ic_passed;
-					}
-				}
-			}
-			if (R.drawable.ic_passed != ic && R.drawable.ic_failed != ic)
-			{
-				ic = R.drawable.ic_maybe;
-			}
-		}
-		catch (IOException ex) {}
-		return setDrawable(R.id.TextView_mnt, ic);
-	}
-
-	// Check if /data/local/deb/bootdeb is available
-	private int checkDeb()
-	{
-		int ic = R.drawable.ic_failed;
-		File file = new File(String.format("%s/local/deb/bootdeb", Environment.getDataDirectory().getAbsolutePath()));
-		if (file.exists())
-		{
-			ic = R.drawable.ic_maybe;
-			ProcessBuilder cmd;
+			// Check if absolute RAM above certain threshold
+			v_mem.ic = R.drawable.ic_unknown;
+			File file = new File("/proc/meminfo");
 			try
 			{
-				String result = "";
-				String[] args = {"/system/bin/ls", "-l", file.getAbsolutePath()};
-				cmd = new ProcessBuilder(args);
-
-	 			Process process = cmd.start();
-	 			InputStream in = process.getInputStream();
-	 			byte[] re = new byte[1024];
-	 			while(in.read(re) != -1)
-	 			{
-	  				result = result + new String(re);
-	 			}
-	 			in.close();
-	 			if (result.startsWith("-rwx"))
-	 			{
-	 				ic = R.drawable.ic_passed;
-	 			}
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				// Example: MemTotal: 2050880 kB
+				Pattern p = Pattern.compile("^MemTotal:\\s+(\\d+)\\s+kB$");
+				Matcher m = p.matcher(br.readLine());
+				if (m.find())
+				{
+					int tm=Integer.parseInt(m.group(1));
+					if (tm < 90000)
+					{
+						v_mem.ic = R.drawable.ic_failed;
+					}
+					else if (tm < 120000)
+					{
+						v_mem.ic = R.drawable.ic_maybe;
+					}
+					else
+					{
+						v_mem.ic = R.drawable.ic_passed;
+					}
+				}
 			}
 			catch(IOException ex)
 			{
 	 			ex.printStackTrace();
 			}
+			publishProgress(v_mem);
+
+			// Check if ext2/3/4 file systems supported
+			v_ext.ic = R.drawable.ic_unknown;
+			file = new File("/proc/filesystems");
+			try
+			{
+				String line;
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				while (null != (line = br.readLine()) && R.drawable.ic_passed != v_ext.ic)
+				{
+					if (line.matches("\\s+ext[234]$"))
+					{
+						v_ext.ic = R.drawable.ic_passed;
+					}
+				}
+				if (R.drawable.ic_passed != v_ext.ic)
+				{
+					v_ext.ic = R.drawable.ic_failed;
+				}
+			}
+			catch(IOException ex)
+			{
+	 			ex.printStackTrace();
+			}
+			publishProgress(v_ext);
+
+			// Check if loop-mounts are supported
+			v_dev.ic = R.drawable.ic_unknown;
+			file = new File("/proc/devices");
+			try
+			{
+				String line;
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				while (null != (line = br.readLine()) && R.drawable.ic_passed != v_dev.ic)
+				{
+					if (line.matches("^\\s+7\\s+loop$"))
+					{
+						v_dev.ic = R.drawable.ic_passed;
+					}
+				}
+				if (R.drawable.ic_passed != v_dev.ic)
+				{
+					v_dev.ic = R.drawable.ic_failed;
+				}
+			}
+			catch(IOException ex)
+			{
+	 			ex.printStackTrace();
+			}
+			publishProgress(v_dev);
+
+			// Check if kernel modules are supported
+			v_mod.ic = R.drawable.ic_failed;
+			file = new File("/proc/modules");
+			if (file.exists())
+			{
+				v_mod.ic = R.drawable.ic_passed;
+			}
+			publishProgress(v_mod);
+
+			// Check if /data is not mounted with noexec
+			v_mnt.ic = R.drawable.ic_unknown;
+			file = new File("/proc/mounts");
+			try
+			{
+				String line;
+				Pattern p = Pattern.compile(String.format("^\\S+\\s+%s\\s+\\S+\\s+(\\S+)", Environment.getDataDirectory().getAbsolutePath()));
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				while (null != (line = br.readLine()) && R.drawable.ic_passed != v_mnt.ic && R.drawable.ic_failed != v_mnt.ic)
+				{
+					Matcher m = p.matcher(line);
+					if (m.find())
+					{
+						int i;
+						String[] opts = m.group(1).split(",");
+						for (i = 0; i < opts.length; i++)
+						{
+							if (opts[i].contentEquals("noexec"))
+							{
+								v_mnt.ic = R.drawable.ic_failed;
+							}
+						}
+						if (R.drawable.ic_failed != v_mnt.ic)
+						{
+							v_mnt.ic = R.drawable.ic_passed;
+						}
+					}
+				}
+				if (R.drawable.ic_passed != v_mnt.ic && R.drawable.ic_failed != v_mnt.ic)
+				{
+					v_mnt.ic = R.drawable.ic_maybe;
+				}
+			}
+			catch(IOException ex)
+			{
+	 			ex.printStackTrace();
+			}
+			publishProgress(v_mnt);
+
+			// Check if /data/local/deb/bootdeb is available
+			v_deb.ic = R.drawable.ic_failed;
+			file = new File(String.format("%s/local/deb/bootdeb", Environment.getDataDirectory().getAbsolutePath()));
+			if (file.exists())
+			{
+				v_deb.ic = R.drawable.ic_maybe;
+				ProcessBuilder cmd;
+				try
+				{
+					String result = "";
+					String[] args = {"/system/bin/ls", "-l", file.getAbsolutePath()};
+					cmd = new ProcessBuilder(args);
+
+		 			Process process = cmd.start();
+		 			InputStream in = process.getInputStream();
+		 			byte[] re = new byte[1024];
+		 			while(in.read(re) != -1)
+		 			{
+		 				result = result + new String(re);
+		 			}
+		 			in.close();
+		 			if (result.startsWith("-rwx"))
+		 			{
+		 				v_deb.ic = R.drawable.ic_passed;
+		 			}
+				}
+				catch(IOException ex)
+				{
+		 			ex.printStackTrace();
+				}
+			}
+			publishProgress(v_deb);
+			return null;
 		}
-		
-		return setDrawable(R.id.TextView_deb, ic);
+
+		protected void onProgressUpdate(Verdict... verdicts) {
+			int i;
+			for (i = 0; i < verdicts.length; i++)
+			{
+				TextView tv = (TextView)findViewById(verdicts[i].tv);
+				tv.setCompoundDrawablesWithIntrinsicBounds(verdicts[i].ic, 0, 0, 0);
+			}
+		}
 	}
 
 	@Override
-	public void onCreate(Bundle savedInstanceState)
+	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		TextView tv = (TextView)findViewById(R.id.TextView_mnt);
 		tv.setText(String.format(getString(R.string.str_mnt), Environment.getDataDirectory().getAbsolutePath()));
-
-		ic_cpu = checkCpu();
-		ic_mem = checkMem();
-		ic_fsys = checkFsys();
-		ic_mod = checkMod();
-		ic_dev = checkDev();
-		ic_mnt = checkMnt();
-		ic_deb = checkDeb();
+		new CourtTask().execute();
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
